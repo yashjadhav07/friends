c777ab935c98bc6d1cd2cf964f5a30a06bccca49
Yash Jadhav <53169635+yashjadhav07@users.noreply.github.com>
2021-06-12 15:40:20 +0530

    Add files via upload

---
+++ src/mock/data.js
@@ -0,0 +1,152 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: "Brother's Forever", // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Time wasted successfully', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi! my name is ',
+  name: 'Yash',
+  subtitle: "I don't know what have I done in my past life that I got these creatures as my friends. They are awesome. Simply awesome!",
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'yash.jpg',
+  paragraphOne: 'I am the kind of introverted person who binge-watch animes. My personal interest lies in watching suspense thriller shows with a good amount of plot twists. Traveling and exploring new places gives me excitement as it refreshes my mind. Deep learning, Augmented reality, and Web development are my domains of interest.'
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'vishal.jpg',
+    title: 'Vishal',
+    info: 'An engineer who understands economy & current affairs more than engineering. My school & colony friend who use to take my side even when everyone were against me. Always supportive like a big brother to me. His aspect of viewing a problem was different because he always used to get a top view (because every problem was shorter than him).',
+    info2: '#VC',
+  },
+  {
+    id: nanoid(),
+    img: 'tanmay.jpg',
+    title: 'Tanmay',
+    info: "Bark-Eat-Sleep-Repeat was his daily routine until this underdog started preparing for exams for joining the Indian armed forces. Can't wait for the day when we will all feel proud of you.",
+    info2: '#royal',
+  },
+  {
+    id: nanoid(),
+    img: 'satyam.jpg',
+    title: 'Satyam',
+    info: "A shinigami working part-time as a human. He can cure you with his sense of humor. My anime partner who got lost in search of his waifu. He is like a cockroach that can survive any situation and yes I can tell that from an innocent smile of yours. Our style of greeting is different, 'तुला कोणि बोलवलय' is mandatory.",
+    info2: '#shaitan',
+  },
+  {
+    id: nanoid(),
+    img: 'saurabh.jpg',
+    title: 'Saurabh',
+    info: "Hardwork, Passion, self-motivated.....these words fall short. He will be the first Saurabh Magdum in the field of AI.  Unfortunately cold🤧 keeps him away from our meets. Saurya, Shoab, sho off whatever you call him, he's the radio wave that can penetrate any wall(seriously I can't write what I want to express).",
+    info2: '#infinity',
+  },
+  {
+    id: nanoid(),
+    img: 'shubham.jpeg',
+    title: 'Shubham',
+    info: "A silent killer! He is full of mysteries and hard to predict. Real-life Levi Ackerman who will get your job done at any cost. He's a defender with whom Sergio Ramos was compared(deadly tackles ☠️). モンスタータトゥーであなたを傷つけてごめんなさい.",
+    info2: '#monstor',
+  },
+  {
+    id: nanoid(),
+    img: 'prajwal.jpeg',
+    title: 'Prajwal',
+    info: "He can do anything for his friends, I guess so! The leader of cattle 😂 and one who speaks directly to the point no matter who you are. Most passionate about his field is all I can say. He knows how to keep in touch (only if he cares about the person on another end). Prajwal is like Dum to (chicken) biryani who will the reason for enlighting your day.",
+    info2: '#vamos',
+  },
+  {
+    id: nanoid(),
+    img: 'pravin.jpg',
+    title: 'Pravin',
+    info: "Winner winner chicken dinner🔥. He's the one on whom you can rely. Responsible, Polite, True gentleman, Tsunami Ek Jwalamukhi character who will never leave you back. You know, I know, God knows what I want to say. Most of the remarkable memories are recorded via his camera.",
+    info2: '#shandal',
+  },
+  {
+    id: nanoid(),
+    img: 'prathamesh.jpeg',
+    title: 'Prathamesh',
+    info: "What can I say about this creature (English should learn you). The most dedicated person I saw in my life. Just tell him It's been a while since we met and he will organize a plan for the union. He is the cloud that stores all memories of us in his secret hard disk.",
+    info2: '#serendipity',
+  },
+  {
+    id: nanoid(),
+    img: 'jay.jpg',
+    title: 'Jay',
+    info: "Hehehe...Here comes the center of mass of all jokes. His contribution as a subject for jokes makes him unique. Even if Jay's friend loses all memory in an accident, he/she will never forget who is jay. Always supportive, spreading wisdom (stop forcing me to be a vegetarian).",
+    info2: '#J',
+  },
+  {
+    id: nanoid(),
+    img: 'gaurav.jpg',
+    title: 'Gaurav',
+    info: "O g, This freak is so busy submitting assignments that, one day I would format his laptop. Deshbhakt- believe me, his thoughts and writings can change your point of viewing a country's politics. God gave him a slanty head so that unnecessary critics just slide away from his mind (just like this one (≧▽≦)).",
+    info2: '#/',
+  },
+  {
+    id: nanoid(),
+    img: 'aditya.jpeg',
+    title: 'Aditya',
+    info: "Haha, here we come. He is the catalyst that will not create any moment of joy but, will boost the joy infinite times. He is phenomenal when it comes to watching movies/series/animes but he will still top in exam. When a normal talk moves towards his relationship, he will act as hard as a diamond from outside but, will melt like helium (not again..., why am I including science in literature).",
+    info2: '#dada',
+  },
+  {
+    id: nanoid(),
+    img: 'aadesh.jpg',
+    title: 'Aadesh',
+    info: "Humble, polite, chivalrous man. Die-hard Liverpool fan who never walks alone (...maybe he is afraid😗). We all have one saint-like personality in the group and that's him. DEV- who bestows cake to everyone.",
+    info2: '#YNWA',
+  },
+  {
+    id: nanoid(),
+    img: 'anirudh.jpg',
+    title: 'Anirudh',
+    info: "A superhero but, what are his superpowers? Well, his curiosity to learn new things and eagerness to apply his knowledge for a better life on earth is his prime superpower. Every fortune owner started their career from the storeroom, warehouse, etc but, our Anirudh started from his famous divider :)",
+    info2: '#haan©',
+  },
+  {
+    id: nanoid(),
+    img: 'abhishek.jpg',
+    title: 'Abhishek',
+    info: "I have nothing to say because this guy doesn’t need any introduction. He's a pro coder who has the capacity to hack NASA with HTML. Unfortunately, the only way to communicate with him is by sharing memes.",
+    info2: '#anirudh',
+  }
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: "I don't know how many of you will see this website but, if you do then please give me your feedback.",
+  btn: '',
+  email: 'yash.jadhav18@vit.edu',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/yash-jadhav-41a9a917b',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/yashjadhav07',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
